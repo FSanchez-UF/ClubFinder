746bfb37168d473c6b3b8499a78d8c9091128e0c
FSanchez-UF <73603246+FSanchez-UF@users.noreply.github.com>
2022-04-18 15:46:21 -0400

    Add files via upload

---
+++ ProgramCode/frontend/src/SelectAll.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SelectAll({selectAll}) { // Allows user to select all categories
+  return (
+    <div data-testid='select-all-test' className='select_all_button'>
+        <input type="submit" value="Select All" onClick={selectAll}/>
+      </div>
+  )
+}
